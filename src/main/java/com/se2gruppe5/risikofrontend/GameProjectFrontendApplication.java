32bb8b1ef7fa9384594731f689e6e339c78129a2
Elias Pozewaunig <elias.pozewaunig@aau.at>
2025-03-22 19:04:18 +0100

    Reset project to fix configuration errors

---
+++ src/main/java/com/se2gruppe5/risikofrontend/GameProjectFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.se2gruppe5.risikofrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameProjectFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameProjectFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se2gruppe5/risikofrontend/GameProjectFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se2gruppe5.risikofrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameProjectFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
