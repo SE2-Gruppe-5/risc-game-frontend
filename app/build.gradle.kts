f3ae36f8aa25f30660a7f634bb983eeddb6c7106
Elias Pozewaunig <elias.pozewaunig@aau.at>
2025-03-24 03:29:36 +0100

    Switch to JDK 21 for consistency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     testOptions {
         unitTests {
@@ -40,7 +40,7 @@ android {
         }
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "21"
     }
 }
 
