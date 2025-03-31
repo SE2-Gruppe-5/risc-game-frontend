1238c08a31fc82dea45c1a970ace58dde24ee84b
Webbiii <webbiii@webbiii.cc>
2025-03-31 02:17:59 +0200

    Implement basic chat as main activity

---
+++ app/src/main/java/com/se2gruppe5/risikofrontend/Constants.kt
@@ -0,0 +1,7 @@
+package com.se2gruppe5.risikofrontend
+
+class Constants {
+    companion object {
+        const val SSE_URL = "http://10.0.0.91:8080/sse"
+    }
+}
---
+++ app/src/main/java/com/se2gruppe5/risikofrontend/SSEClient.kt
@@ -0,0 +1,21 @@
+package com.se2gruppe5.risikofrontend
+
+import com.launchdarkly.eventsource.EventHandler
+import com.launchdarkly.eventsource.EventSource
+import java.net.URI
+import java.time.Duration
+
+class SSEClient {
+    private var eventHandler: EventHandler? = null
+    private var eventSource: EventSource? = null;
+
+    fun init(eventHandler: EventHandler) {
+        this.eventHandler = eventHandler
+
+        eventSource = EventSource.Builder(eventHandler, URI.create(Constants.SSE_URL))
+            .connectTimeout(Duration.ofSeconds(10))
+            .backoffResetThreshold(Duration.ofSeconds(10))
+            .build()
+        eventSource!!.start()
+    }
+}
