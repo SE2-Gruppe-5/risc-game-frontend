ee24ddf44482938d5961b98e8c8c10ecc68a6f84
Markus-M-AAU <markusmue@edu.aau.at>
2025-04-22 15:11:54 +0200

    Added Territory Interfacing Example to Main Menu

--- app/src/main/java/com/se2gruppe5/risikofrontend/startmenu/MenuActivity.kt
+++ app/src/main/java/com/se2gruppe5/risikofrontend/startmenu/MenuActivity.kt
@@ -2,6 +2,7 @@ package com.se2gruppe5.risikofrontend.startmenu
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -9,6 +10,11 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.se2gruppe5.risikofrontend.R
+import com.se2gruppe5.risikofrontend.game.territoryIO.ITerritoryUIWrapper
+import com.se2gruppe5.risikofrontend.game.territoryIO.TerritoryManager
+import com.se2gruppe5.risikofrontend.game.territoryIO.TerritoryUIAndroid
+import com.se2gruppe5.risikofrontend.game.dataclasses.Player
+import com.se2gruppe5.risikofrontend.game.dataclasses.Territory
 import com.se2gruppe5.risikofrontend.game.interactables.DiceButton
 import com.se2gruppe5.risikofrontend.lobby.CreateLobbyActivity
 import com.se2gruppe5.risikofrontend.lobby.JoinLobbyActivity
@@ -38,10 +44,35 @@ class MenuActivity : AppCompatActivity() {
         tutorialBtn.setOnClickListener({
             Log.i("NAVIGATION", "Entering Tutorial")
         })
+
         //Placeholder
         val diceBtn = this.findViewById<ImageButton>(R.id.diceButton)
         val diceTxt = this.findViewById<TextView>(R.id.diceText)
         DiceButton(diceBtn,diceTxt,1,6)
+        //-------------
+
+        //Playerholder
+
+        val p1 = Player(1,"Markus", Color.rgb(255,100,0))
+        val p2 = Player(2, "Leo", Color.rgb(0,100,255))
+
+        val t1_txt = this.findViewById<TextView>(R.id.territoryAtext)
+        val t1_btn = this.findViewById<ImageButton>(R.id.territoryAbtn)
+        val t1_vis: ITerritoryUIWrapper = TerritoryUIAndroid(t1_txt,t1_txt,t1_btn)
+        val t1 = Territory(1, t1_vis)
+
+        val t2_txt = this.findViewById<TextView>(R.id.territoryBtext)
+        val t2_btn = this.findViewById<ImageButton>(R.id.territoryBbtn)
+        val t2_vis: ITerritoryUIWrapper = TerritoryUIAndroid(t2_txt,t2_txt,t2_btn)
+        val t2 = Territory(2, t2_vis)
+
+        val territoryManager: TerritoryManager = TerritoryManager()
+        territoryManager.addTerritory(t1)
+        territoryManager.addTerritory(t2)
+
+        territoryManager.assignOwner(t1,p1)
+        territoryManager.assignOwner(t2,p2)
+
         //-------------
     }
 
