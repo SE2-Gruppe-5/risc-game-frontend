fa1e809de23f204a1691addec6851d53748a18fa
Markus-M-AAU <markusmue@edu.aau.at>
2025-04-24 23:22:41 +0200

    Moved Features to GameActivity
    (where they belong)
    And Added shortcut "go to game view" button for testing

--- app/src/main/java/com/se2gruppe5/risikofrontend/startmenu/MenuActivity.kt
+++ app/src/main/java/com/se2gruppe5/risikofrontend/startmenu/MenuActivity.kt
@@ -14,6 +14,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.toColorInt
 import com.se2gruppe5.risikofrontend.R
+import com.se2gruppe5.risikofrontend.game.GameActivity
 import com.se2gruppe5.risikofrontend.game.territory.ITerritoryVisual
 import com.se2gruppe5.risikofrontend.game.managers.TerritoryManager
 import com.se2gruppe5.risikofrontend.game.territory.TerritoryVisualAndroid
@@ -47,56 +48,16 @@ class MenuActivity : AppCompatActivity() {
             startActivity(intent)
         })
 
+        val debugBTN = this.findViewById<Button>(R.id.debugme)
+        debugBTN.setOnClickListener({
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        })
+
         tutorialBtn.setOnClickListener({
             Log.i("NAVIGATION", "Entering Tutorial")
         })
 
-        //Placeholder
-        val diceBtn = this.findViewById<ImageButton>(R.id.diceButton)
-        val diceTxt = this.findViewById<TextView>(R.id.diceText)
-        val diceVisualAndroid = DiceVisualAndroid(Dice1d6Generic(),diceBtn,diceTxt)
-        diceVisualAndroid.clickSubscription {it.roll()}
-
-        val p1 = PlayerRecord(1,"Markus", Color.rgb(255,100,0))
-        val p2 = PlayerRecord(2, "Leo", Color.rgb(0,100,255))
-
-        val t1 = TerritoryRecord(1)
-        val t1_txt = this.findViewById<TextView>(R.id.territoryAtext)
-        val t1_btn = this.findViewById<ImageButton>(R.id.territoryAbtn)
-        val t1_outline = this.findViewById<View>(R.id.territoryAoutline)
-        val t1_vis: ITerritoryVisual = TerritoryVisualAndroid(t1, t1_txt,t1_txt,t1_btn,t1_outline)
-
-        val t2 = TerritoryRecord(2)
-        val t2_txt = this.findViewById<TextView>(R.id.territoryBtext)
-        val t2_btn = this.findViewById<ImageButton>(R.id.territoryBbtn)
-        val t2_outline = this.findViewById<View>(R.id.territoryBoutline)
-        val t2_vis: ITerritoryVisual = TerritoryVisualAndroid(t2,t2_txt,t2_txt,t2_btn,t2_outline)
-
-        val pointingArrow = PointingArrowAndroid(this,"#FF0000".toColorInt(),15f)
-        pointingArrow.layoutParams = ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT
-        )
-        findViewById<ViewGroup>(R.id.main).addView(pointingArrow)
-
-        TerritoryManager.init(p2,pointingArrow)
-        val territoryManager = TerritoryManager.get()
-        territoryManager.addTerritory(t1_vis)
-        territoryManager.addTerritory(t2_vis)
-
-        territoryManager.assignOwner(t1_vis,p1)
-        territoryManager.assignOwner(t2_vis,p2)
-
-
-        this.findViewById<ImageButton>(R.id.goattack).setOnClickListener({
-            territoryManager.enterSelectMode()
-            territoryManager.enterAttackMode()
-            this.findViewById<ImageButton>(R.id.goattack).backgroundTintList = ColorStateList.valueOf(Color.RED)
-        })
-
-
-
-        //-------------
     }
 
 
