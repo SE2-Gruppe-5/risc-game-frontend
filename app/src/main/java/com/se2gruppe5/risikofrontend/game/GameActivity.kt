fd32d0453de577ed7b741cba157bc55c7479165d
MichaelSereinig <michael.sereinig@gmx.at>
2025-05-05 18:17:17 +0200

    Hinzufügen des Bonusscreens zur Game.activity.

--- app/src/main/java/com/se2gruppe5/risikofrontend/game/GameActivity.kt
+++ app/src/main/java/com/se2gruppe5/risikofrontend/game/GameActivity.kt
@@ -1,8 +1,10 @@
 package com.se2gruppe5.risikofrontend.game
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.se2gruppe5.risikofrontend.Popup.ContinentDialog
 import com.se2gruppe5.risikofrontend.R
 
 class GameActivity :AppCompatActivity() {
@@ -10,5 +12,14 @@ class GameActivity :AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.game)
+
+        val showContinentButton: Button = findViewById(R.id.btn_show_continents)
+        showContinentButton.setOnClickListener {
+            showContinentDialog()
+        }
+    }
+    fun showContinentDialog() {
+        val dialog = ContinentDialog()
+        dialog.show(supportFragmentManager, "ContinentDialog")
     }
 }
