f6b23f1056e7d17fb14bb63e7e74c2bec0a72c1f
Webbiii <webbiii@webbiii.cc>
2025-05-11 03:38:40 +0200

    Can now get either Map or List of Players

--- app/src/main/java/com/se2gruppe5/risikofrontend/network/sse/messages/UpdatePlayersMessage.kt
+++ app/src/main/java/com/se2gruppe5/risikofrontend/network/sse/messages/UpdatePlayersMessage.kt
@@ -4,4 +4,8 @@ import com.se2gruppe5.risikofrontend.game.dataclasses.PlayerRecord
 import com.se2gruppe5.risikofrontend.network.sse.IMessage
 import java.util.UUID
 
-data class UpdatePlayersMessage(val players: HashMap<UUID, PlayerRecord>) : IMessage
+data class UpdatePlayersMessage(val players: List<PlayerRecord>) : IMessage {
+    fun playersMap(): Map<UUID, PlayerRecord> {
+        return players.associateBy { it.id }
+    }
+}
