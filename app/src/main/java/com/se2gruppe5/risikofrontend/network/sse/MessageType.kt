e420103716a0aede800a646545a9a0faf98d22f6
Webbiii <webbiii@webbiii.cc>
2025-05-11 03:17:35 +0200

    try to automatically deserialize json

--- app/src/main/java/com/se2gruppe5/risikofrontend/network/sse/MessageType.kt
+++ app/src/main/java/com/se2gruppe5/risikofrontend/network/sse/MessageType.kt
@@ -1,51 +1,24 @@
 package com.se2gruppe5.risikofrontend.network.sse
 
-import com.google.gson.JsonObject
+import com.se2gruppe5.risikofrontend.network.sse.messages.ChangeTerritoryMessage
 import com.se2gruppe5.risikofrontend.network.sse.messages.ChatMessage
 import com.se2gruppe5.risikofrontend.network.sse.messages.GameStartMessage
 import com.se2gruppe5.risikofrontend.network.sse.messages.JoinLobbyMessage
 import com.se2gruppe5.risikofrontend.network.sse.messages.LeaveLobbyMessage
 import com.se2gruppe5.risikofrontend.network.sse.messages.SetUuidMessage
-import java.util.UUID
-import java.util.function.Function
+import com.se2gruppe5.risikofrontend.network.sse.messages.UpdatePhaseMessage
+import com.se2gruppe5.risikofrontend.network.sse.messages.UpdatePlayersMessage
+import kotlin.reflect.KClass
 
-enum class MessageType(private val deserializer: Function<JsonObject, IMessage?>) {
-    SET_UUID(
-        Function { data: JsonObject ->
-            SetUuidMessage(UUID.fromString(data.get("uuid")?.asString))
-        }
-    ),
-    CHAT(
-        Function { data: JsonObject ->
-            ChatMessage(data.get("message")?.asString!!)
-        }
-    ),
-    JOIN_LOBBY(
-        Function { data: JsonObject ->
-            JoinLobbyMessage(
-                UUID.fromString(data.get("uuid")?.asString),
-                data.get("playerName")?.asString!!,
-                data.get("lobbyCode")?.asString!!
-            )
-        }
-    ),
-    LEAVE_LOBBY(
-        Function { data: JsonObject ->
-            LeaveLobbyMessage(UUID.fromString(data.get("uuid")?.asString))
-        }
-    ),
-    GAME_START(
-        Function { data: JsonObject ->
-            GameStartMessage(UUID.fromString(data.get("gameId")?.asString))
-        }
-    ),
-    UPDATE_PHASE,
-    UPDATE_PLAYERS,
-    UPDATE_TERRITORIES;
-
-    fun deserialize(data: JsonObject): IMessage? {
-        return deserializer.apply(data)
-    }
+enum class MessageType(val messageClass: KClass<out IMessage>) {
+    SET_UUID(SetUuidMessage::class),
+    CHAT(ChatMessage::class),
+    JOIN_LOBBY(JoinLobbyMessage::class),
+    LEAVE_LOBBY(LeaveLobbyMessage::class),
+    GAME_START(GameStartMessage::class),
+    UPDATE_PHASE(UpdatePhaseMessage::class),
+    UPDATE_PLAYERS(UpdatePlayersMessage::class),
+    UPDATE_TERRITORIES(ChangeTerritoryMessage::class);
 }
 
 fun getMessageType(type: String): MessageType? {
