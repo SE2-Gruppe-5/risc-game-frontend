9c3c8b43cf8b0b28e0dfb21f6f003c765848804f
Webbiii <webbiii@webbiii.cc>
2025-06-24 00:30:12 +0200

    Add reason field

--- app/src/main/java/com/se2gruppe5/risikofrontend/network/sse/messages/LeaveLobbyMessage.kt
+++ app/src/main/java/com/se2gruppe5/risikofrontend/network/sse/messages/LeaveLobbyMessage.kt
@@ -3,4 +3,4 @@ package com.se2gruppe5.risikofrontend.network.sse.messages
 import com.se2gruppe5.risikofrontend.network.sse.IMessage
 import java.util.UUID
 
-data class LeaveLobbyMessage(val uuid: UUID) : IMessage
+data class LeaveLobbyMessage(val uuid: UUID, val reason: String?) : IMessage
